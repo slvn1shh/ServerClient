66bf354cbdb42e9b17db9f6b2057ee2066f65e9b
slvn1shh <by.fast1k@gmail.com>
2019-06-12 16:05:14 +0300

    Add build scripts

---
+++ buildserverandseveralclients/build-and-run(server).bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem set JAVA_HOME=%cd%\tools\jdk-12.0.1
+rem set M2_HOME=%cd%\tools\maven-3.6.1
+rem set PATH="%PATH%;%JAVA_HOME%\bin;%M2_HOME%\bin;
+
+mvn compile && mvn exec:java -Dexec.mainClass="com.serverclientapp.ChatServer"
